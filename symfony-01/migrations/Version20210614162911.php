4a667a02158c51b904c450147a8f15695ea11a59
Marcin Polonis <polonis.marcin@gmail.com>
2021-06-14 18:49:08 +0200

    Create entity, create controllers

---
+++ symfony-01/migrations/Version20210614162911.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210614162911 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE customers ADD name VARCHAR(200) NOT NULL, ADD surname VARCHAR(200) NOT NULL, ADD number VARCHAR(20) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE customers DROP name, DROP surname, DROP number');
+    }
+}
--- symfony-01/src/Entity/Customers.php
+++ symfony-01/src/Entity/Customers.php
@@ -17,8 +17,59 @@ class Customers
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $number;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): self
+    {
+        $this->number = $number;
+
+        return $this;
     }
 }
